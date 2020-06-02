cc5ca7da64ee489591086a27d5bd11317cdcf0f3
heyhurricane <dinohka@mail.ru>
2020-06-02 13:38:47 +0400

    add search and basket to index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,52 @@
 $(document).ready(function () {
+  var basketActive = false;
+  var searchActive = false;
+
+  //выпадающая корзина в секции Hero
+
+  var basket = $('.modalbasket'),
+      basketBtn = $('[data-toggle=basket]');
   
+  basketBtn.on('click', function () {
+    console.log('yes');
+   basket.toggleClass('modalbasket--visible');
+   basketActive = !(basketActive);
+  //  $(".shopping-cart").fadeToggle( "fast");
+  });
+
+  //выпадающий поиск в секции Hero
+
+  var search = $('.modalsearch'),
+      searchBtn = $('[data-toggle=search]');
+      closeBtn =  $('.modalsearch__close');
+  
+  searchBtn.on('click', function () {
+    console.log('yes');
+    search.toggleClass('modalsearch--visible');
+    searchActive = !(searchActive);
+  });
+
+  closeBtn.on('click', function () {
+    search.toggleClass('modalsearch--visible');
+    searchActive = !(searchActive);
+    $('.modalsearch__form')[0].reset();
+  });
+
+  $(document).keydown(function(event) { 
+    if (event.key=='Escape' && (searchActive)) { 
+      search.toggleClass('modalsearch--visible');
+      searchActive = !(searchActive);
+      $('.modalsearch__form')[0].reset();
+    }
+    else {
+      if (event.key=='Escape' && (basketActive)) { 
+        basket.toggleClass('modalbasket--visible');
+        basketActive = !(basketActive);
+      }
+
+    }
+  });
+
   // Слайдер в секции Hero
   var mySwiper = new Swiper ('.heroswiper-container', {
     // Optional parameters
